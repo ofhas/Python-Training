eecbf22b1fad112992a53a9f8fa30091f5934470
ofhas <53121171+ofhas@users.noreply.github.com>
2021-02-07 09:27:13 +0200

    Update Pass_Gen_GUI.py

--- Password Generator./Pass_Gen_GUI.py
+++ Password Generator./Pass_Gen_GUI.py
@@ -1,7 +1,7 @@
+#!/usr/bin/env python3
 from tkinter import *
 import tkinter.messagebox
 import random
-from tkinter.ttk import Combobox
 import pyperclip
 
 root = Tk()
@@ -15,41 +15,91 @@ var1 = IntVar()
 def exit1():
     exit()
 
+
+def digits(length):
+    digitsString = []
+    for i in range(48, 58):
+        digitsString.append(chr(i))
+    digitsString = random.choices(digitsString, k=length)
+    return "".join(digitsString)
+
+
+def smallLetters(length):
+    smallLetterString = []
+    for i in range(97, 123):
+        smallLetterString.append(chr(i))
+    smallLetterString = random.choices(smallLetterString, k=length)
+    return "".join(smallLetterString)
+
+
+def upperCaseLetters(length):
+    upperCaseString = []
+    for i in range(65, 91):
+        upperCaseString.append(chr(i))
+    upperCaseString = random.choices(upperCaseString, k=length)
+    return "".join(upperCaseString)
+
+
+def upperAndLowerCase(length):
+    upperAndLower = []
+    for i in range(97, 123):
+        upperAndLower.append(chr(i))
+    for i in range(65, 91):
+        upperAndLower.append(chr(i))
+    upperAndLower = random.choices(upperAndLower, k=length)
+    return "".join(upperAndLower)
+
+
+def upperLowerNumbers(length):
+    upperLowerAndNumbers = []
+    for i in range(97, 123):
+        upperLowerAndNumbers.append(chr(i))
+    for i in range(65, 91):
+        upperLowerAndNumbers.append(chr(i))
+    for i in range(48, 58):
+        upperLowerAndNumbers.append(chr(i))
+    upperLowerAndNumbers = random.choices(upperLowerAndNumbers, k=length)
+    return "".join(upperLowerAndNumbers)
+
+
+def allChars(length):
+    allCharString = []
+    for i in range(33, 127):
+        allCharString.append(chr(i))
+    allCharString = random.choices(allCharString, k=length)
+    return "".join(allCharString)
+
+
 def pass_generator():
-    entry.delete(0, END) # in order to delete the previous password that was generated in case the output isn't desirable
+    entry.delete(0,
+                 END)  # in order to delete the previous password that was generated in case the output isn't desirable
 
     length = var1.get()
-    digits_only = "0123456789"
-    small_letters = "abcdefghijklmnopqrstuvwxyz"
-    upper_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    upper_and_small = "ABCDEFGpqrIJKHdefstuvwxyzQRSTUghijklmnoLMNOPabcVWXYZ"
-    upper_small_numbers = "ABCDEF567STUst38auvJKjkVwxyz012bcdeflQR49"
-    all_types = "JKLM45678NOPQR3ST29~!fghiUVWX)_+|}{CYZabmnopqAB(DEFGHIrstuvwxyz01jkl@#$%^&*[]'/.,`"
 
     new_length = int(length)
 
     if var.get() == 1:
-        password = random.choices(digits_only, k=new_length)
+        password = digits(new_length)
         return "".join(password)
 
     if var.get() == 2:
-        password = random.choices(small_letters, k=new_length)
+        password = smallLetters(new_length)
         return "".join(password)
 
     if var.get() == 3:
-        password = random.choices(upper_letters, k=new_length)
+        password = upperCaseLetters(new_length)
         return "".join(password)
 
     if var.get() == 4:
-        password = random.choices(upper_and_small, k=new_length)
+        password = upperAndLowerCase(new_length)
         return "".join(password)
 
     if var.get() == 5:
-        password = random.choices(upper_small_numbers, k=new_length)
+        password = upperLowerNumbers(new_length)
         return "".join(password)
 
     if var.get() == 6:
-        password = random.choices(all_types, k=new_length)
+        password = allChars(new_length)
         return "".join(password)
 
 
@@ -64,14 +114,15 @@ def copy1():
 
 
 def instructions():
-    tkinter.messagebox.showinfo("Types of passwords on the scale:", "1: Digits\n2: Small letters\n3: Upper letters\n4: Upper & small letters\n"
-    "5: Upper & small letters & digits\n6: All characters ")
+    tkinter.messagebox.showinfo("Types of passwords on the scale:",
+                                "1: Digits\n2: Small letters\n3: Upper letters\n4: Upper & small letters\n"
+                                "5: Upper & small letters & digits\n6: All characters ")
 
 
-label1 = Label(root, text = "Choose password type:")
+label1 = Label(root, text="Choose password type:")
 label1.place(x=50, y=75)
 
-label2 = Label(root, text = "Weak to strong:")
+label2 = Label(root, text="Weak to strong:")
 label2.place(x=185, y=40)
 
 Random_password = Label(root, text="Password:")
@@ -79,15 +130,14 @@ Random_password.place(x=105, y=200)
 entry = Entry(root)
 entry.place(x=175, y=200)
 
-generate_button = Button(root, text="Generate",width = 12, bg="green", fg="white", command=generate)
-generate_button.place(x=190 , y=160)
+generate_button = Button(root, text="Generate", width=12, bg="green", fg="white", command=generate)
+generate_button.place(x=190, y=160)
 
 copy_button = Button(root, text="Copy", command=copy1)
-copy_button.place(x = 305, y=197)
+copy_button.place(x=305, y=197)
 
 label3 = Label(root, text="Length of password:")
 label3.place(x=50, y=120)
-
 
 password_type = Scale(root, from_=1, to=6, orient=HORIZONTAL, variable=var)
 password_type.place(x=190, y=60)
@@ -101,7 +151,5 @@ menu = Menu(root)
 root.config(menu=menu)
 
 menu.add_command(label="Instructions", command=instructions)
-
-
 
 root.mainloop()
